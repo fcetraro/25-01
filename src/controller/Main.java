5e3b6c97dfb0c97c407a0fe20d975b292a90490b
Federico Cetraro <>
2021-01-25 15:24:47 -0300

    exercise 1 with Pablo changes

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -11,14 +11,16 @@ public class Main {
         Persona p0 = new Persona("Federico Cetraro", 123);
         Persona p1 = new Persona("Carlos Patricio", 121);
         Persona p2 = new Persona("Tomas Merencio", 321);
-        Persona[] toSort = new Persona[3];
+        Persona p3 = new Persona("Pablo", 503);
+        Persona[] toSort = new Persona[4];
         toSort[0]=p0;
         toSort[1]=p1;
         toSort[2]=p2;
+        toSort[3]=p3;
         System.out.println("BEFORE SORT");
         printArray(toSort);
         System.out.println("SORTED");
-        printArray(SortUtil.ordenar(toSort));
+        printArray(SortUtil.ordenarT(toSort));
     }
     private static void printArray(Object[] array){
         for (int i = 0; i < array.length; i++) {
--- src/util/SortUtil.java
+++ src/util/SortUtil.java
@@ -17,4 +17,18 @@ public class SortUtil {
         }
         return arr;
     }
+    public static <T> Precedable[] ordenarT(Precedable<T> arr[]){
+        int i, j;
+        Precedable aux;
+        for (i = 0; i < arr.length - 1; i++) {
+            for (j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j + 1].precedeA((T)arr[j]) == 1) {
+                    aux = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = aux;
+                }
+            }
+        }
+        return arr;
+    }
 }
